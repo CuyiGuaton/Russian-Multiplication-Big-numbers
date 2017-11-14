afa0dd4e026917e40a97bddabbbcd00b876008b5
Sergio <sergio.salinas@usach.cl>
2017-11-14 00:23:10 -0300

    primer test

--- test1.c
+++ test1.c
@@ -3,15 +3,26 @@
 #include <string.h>
 #include <math.h>
 #include <gmp.h>
+#include <time.h>
+
+#define I(a) (a - '0')
 #define MAX 400000000
 //para funcionar se debe cambiar el main de a.c a int main(mpt_z n) y borrar las declaración, iniciacion y liberación de n
+
+
+void longdiv(const char *a, char *c);
+void longmulti(const char *a, char *c);
+void longsum(const char * a,  const char * b, char * c);
+double russianmul(char *a, char *b);
+
 
 int main(int argc, char const *argv[]) {
     
    	unsigned long long int largo, exp, i;
+    double time;
 	char *a = (char *)malloc(MAX);
 	char *b = (char *)malloc(MAX);
-    char *command = (char *)malloc(MAX*2);
+    char *command = (char *)malloc(100);
   	system(  "echo -n "" > result1.txt");
 
 
@@ -28,21 +39,23 @@ int main(int argc, char const *argv[]) {
         //gmp_printf ("%i %Zd %Zd\n", i,n,m);
 
 		largo = pow(2,i) - 1;			// Calculamos el largo del numero.
-   		memset(a, '0', largo);
-     	a[largo+1] = '\0';
+   		//memset(a, '0', largo);
+     	//a[largo+1] = '\0';
 
-		memset(b, '0', largo);
-		b[largo+1] = '\0';
+//		memset(b, '0', largo);
+	//	b[largo+1] = '\0';
         
         mpz_get_str(a,10,n); 
         mpz_get_str(b,10,m); 
         
-//        printf("%llu %s %s\n    ", i,a,b);
-        memset(command, '0', largo);
-          
-       	snprintf ( command, MAX,  "echo  %llu $(./bfnp  %s %s \n)  >> result1.txt", largo, a,b );
-     	system(command);
 
+
+        russianmul(a,b);
+        printf("%llu %s %s\n    ", i,a,b);
+        time = russianmul(a,b);
+     	snprintf ( command, 100,  "echo  %llu %f  >> result1.txt", i, time );
+
+     	system(command);
 
     }
     gmp_randclear(state);
@@ -52,4 +65,116 @@ int main(int argc, char const *argv[]) {
     free(b);
     free(command);
     return 0;
+}
+
+
+double russianmul(char *a, char *b) {
+	//Declaración  de variables
+	int la = strlen(a);
+    int lb = strlen(b);
+	char *aux =(char *)malloc(la+1);
+	char *aux2 = (char *)malloc(lb+MAX+1);
+    char *result= (char *)malloc((la+lb)+MAX); //RESULTADO
+	char *aux3 = (char *)malloc((la+lb)+MAX);
+
+    
+    clock_t start, end;
+    start = clock();
+    
+    double elapsed_time;
+	//Ciclo Multiplicación rusa
+	while (a[0]) { //Mientras a no sea 0
+		if( I(a[strlen(a)-1]) % 2){ //si a es impar
+				longsum(b,result,aux3); //Se suma el primer segundo número por el resultado
+				strcpy(result,aux3);
+		}
+		longdiv(a,aux); //se divide el primer número por 2
+		strcpy(a,aux);
+		longmulti(b,aux2); //Se multiplica el segundo número por dos
+		strcpy(b,aux2);
+	}
+
+	end = clock();
+	elapsed_time = (end - start)/(double)CLOCKS_PER_SEC;
+	//printf("%f largo a= %i largo b = %i", elapsed_time, la , lb);
+//    printf("%f", elapsed_time);
+	//printf("\n Resultado = %s\n", result);
+
+	//Liberación memoria
+	free(result);
+	free(aux);
+	free(aux2);
+	free(aux3);
+
+    return elapsed_time;
+}
+
+
+void longdiv(const char *a, char *c){
+    int la = strlen(a);
+    memset(c, '0', la);
+    c[la] = '\0';
+    int carry = 0;
+    int n=0;
+    int i;
+    for(i=0; i<la; i=i+1){
+        n = (carry*10 + I(a[i]))/2; //se divide un número de dos digitos, el primero es el carry y el segundo a dividir
+        carry = (carry*10 + I(a[i])) - 2*n; //se calcula el resto de la división y se deja como carry
+        c[i] = n + '0';
+    }
+    if(c[0] == '0'){ //se elimina el cero que quede adelante
+  			 memmove(c, c + 1, la);
+  	     c[la] = '\0';
+    }
+
+}
+
+void longmulti(const char *a, char *c){
+	int i = 0, k = 0, carry, n, la;
+
+	la = strlen(a);
+	memset(c, '0', la + 1);
+	c[la + 2] = '\0';
+	for (i = la - 1, k = i + 1; i >= 0; i--){
+
+        n = I(a[i]) * 2 + I(c[k]);
+        carry = n / 10;
+        c[k] = (n % 10) + '0';
+        k--;
+				c[k] += carry;
+	}
+
+  if(c[0] == '0'){
+			memmove(c, c + 1, la);
+	     c[la] = '\0';
+     }
+}
+
+
+void longsum(const char * a,  const char * b, char * c){
+  int la = strlen(a);
+  int lb = strlen(b);
+  int carry = 0;
+  int sum;
+  memset(c, '0', la+1); //llena c de 0
+	//Crea un arreglo aux con el mismo largo que a pero con los valores de b y lleno adelante con ceros
+	char *aux= (char *)malloc(la+2);
+	memset(aux, '0', la-lb);
+    aux[la-lb] = '\0';
+	strcat(aux,b);
+  while( la >  0) {
+      sum = I(a[la-1]) + I(aux[la-1]) + carry; // se suman dos digitos más el carry
+      carry = sum/10; //si es mayor a 10 el carry es zero
+      c[la] = sum%10 + '0'; // se guarda el primer digito de la suma como resultado
+      la--;
+  }
+	if (carry >0) { //si la última suma es mayor a 10 entonces
+		c[0] = carry + '0';
+	}
+	la = lb = strlen(a);
+  if(c[0] == '0'){ //se eliminan los ceros de adelante
+      memmove(c, c + 1, la);
+       c[la] = '\0';
+     }
+    free(aux);
 }
